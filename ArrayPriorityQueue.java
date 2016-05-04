5547c0372ea1a12be5187cb52a74f94d4932c2f3
anna tolen <atolen@stuy.edu>
2016-05-04 15:02:40 -0400

    not compiling, still have to clear up object stuff

--- ArrayPriorityQueue.java
+++ ArrayPriorityQueue.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
 
-public class ArrayPriorityQueue<T> {
+public class ArrayPriorityQueue implements PriorityQueue {
 
-    private ArrayList<T> _data;
+    private ArrayList _data;
     
     public ArrayPriorityQueue() {
-	_data = new ArrayList<T>(10);
+	_data = new ArrayList(10);
     }
 
-    public void add( T x ) {
+    public void add( Object x ) {
 	if( isEmpty() ) {
 	    _data.add(x);
 	    return;
@@ -24,12 +24,12 @@ public class ArrayPriorityQueue<T> {
 	return _data.size() == 0;
     }
 
-    public T peekMin() {
+    public Object peekMin() {
 	return _data.get(0);
     }
 
-    public T removeMin() {
-	T tmp = _data.get(0);
+    public Object removeMin() {
+	Object tmp = _data.get(0);
 	_data.remove(0);
 	return tmp;
     }
