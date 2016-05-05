066bdbc622008cd60751568642f08c7e150b62c7
lucytang4 <ltang@stuy.edu>
2016-05-05 00:41:16 +0000

    compiles, just need main

--- ArrayPriorityQueue.java
+++ ArrayPriorityQueue.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
 
-public class ArrayPriorityQueue implements PriorityQueue {
+public class ArrayPriorityQueue implements PriorityQueue{
 
-    private ArrayList _data;
+    private ArrayList<Comparable> _data;
     
     public ArrayPriorityQueue() {
-	_data = new ArrayList(10);
+	_data = new ArrayList<Comparable>();
     }
 
-    public void add( Object x ) {
+    public void add( Comparable x ) {
 	if( isEmpty() ) {
 	    _data.add(x);
 	    return;
@@ -24,14 +24,23 @@ public class ArrayPriorityQueue implements PriorityQueue {
 	return _data.size() == 0;
     }
 
-    public Object peekMin() {
+    public Comparable peekMin() {
 	return _data.get(0);
     }
 
-    public Object removeMin() {
-	Object tmp = _data.get(0);
+    public Comparable removeMin() {
+	Comparable tmp = _data.get(0);
 	_data.remove(0);
 	return tmp;
+    }
+    
+    
+    public String toString(){
+        String s = "[";
+        for (int i = 0; i < _data.size(); i++){
+            s += _data.get(i) + ",";
+        }
+        return s + "]";
     }
 
     public static void main( String[] args ) {
--- PriorityQueue.java
+++ PriorityQueue.java
@@ -1,12 +1,12 @@
 public interface PriorityQueue {
 
-    public void add( Object x ); //adds an item to this priority queue
+    public void add( Comparable x ); //adds an item to this priority queue
 
     public boolean isEmpty(); //returns true if stack is empty, otherwise returns false
 
-    public Object peekMin(); //returns smallist item stored in this priority queue without removing it
+    public Comparable peekMin(); //returns smallist item stored in this priority queue without removing it
 
-    public Object removeMin(); //removes and returns the smallest item stored in this priority queue
+    public Comparable removeMin(); //removes and returns the smallest item stored in this priority queue
 
     //smallest item == highest priority
 
